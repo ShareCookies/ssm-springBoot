3b8e35dc8bca1fddf908d939fd13d2ac5000b05d
hecaigui <982791027@qq.com>
2020-06-14 13:34:13 +0800

    feat：1.springCloudConfig回顾2.restTemplate整理

---
+++ 微服务/配置中心/例/spring-cloud-config-server/src/main/java/com/neo/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.neo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+//激活对注册中心的支持
+@EnableDiscoveryClient
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
