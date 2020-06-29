4a57f790a91f625bfd2d3af3b028b040d2a5cce5
hecaigui <982791027@qq.com>
2020-06-29 23:15:52 +0800

    feat：初整springBoot

--- springBoot/Application.java
+++ springBoot/Application.java
@@ -1,105 +1,65 @@
+package com.china.hcg.eas.business;
 
-import com.dangdang.ddframe.job.config.JobCoreConfiguration;
-import com.dangdang.ddframe.job.config.simple.SimpleJobConfiguration;
-import com.dangdang.ddframe.job.lite.config.LiteJobConfiguration;
-import com.dangdang.ddframe.job.lite.spring.api.SpringJobScheduler;
-import com.dangdang.ddframe.job.reg.base.CoordinatorRegistryCenter;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import redis.clients.jedis.JedisPoolConfig;
 
-import java.nio.charset.Charset;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
- * @author 
+ * @author
+ * <p>
+ * 该类的作用是提供spring配置
+ * @Configuration 标识该类是一个spring配置类，该配置类会被springBoot自动扫描并加载
+ * @ComponentScan 默认扫描该包及子包下的spring的组件
+ * @MappserScan 扫描指定包的mybatis mapper
+ * @EnableTransactionManagement 启用事务管理
+ * @EnableCaching 启用缓存
+ * @EnableAutoConfiguration 启用spring boot的自动配置
  */
 @Configuration
 @ComponentScan
-@EnableAutoConfiguration
+//@MapperScan({"com.china.hcg.eas.business.*.mapper"})
 @EnableTransactionManagement
 @EnableCaching
-@EnableScheduling
-@MapperScan({"com.ttt.egov.zjsgy.platform.mapper"})
-@Import({UserWebConfiguration.class, DocWebConfiguration.class, UrgerWebConfiguration.class,
-        InfoWebConfiguration.class, MessageClientBusinessConfiguration.class, MessageWebConfiguration.class,
-        MeetingWebConfiguration.class, RequestWebConfiguration.class, NoticeWebConfiguration.class, ApprovalWebConfiguration.class, AuditConfiguration.class,
-        SolrDataWebConfiguration.class, ReportWebConfiguration.class, ElasticJobConfiguration.class, FlowEventListenerApplication.class, AuditLogBusinessConfiguration.class
-})
+@EnableAutoConfiguration
+// 导入别的jar包（pom引入的项目）的配置文件
+//@Import({UserWebConfiguration.class})
 public class Application {
-
-    public static final Logger logger = LoggerFactory.getLogger(Application.class);
-
     public static void main(String[] args) {
-        logger.info("application default encoding: {}", System.getProperty("file.encoding"));
-        logger.info("application default character encoding: {}", Charset.defaultCharset().name());
-        logger.info("application default language: {}", System.getProperty("user.language"));
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    IgnoredPathsWrapper oaServiceWebIgnoredPathsWrapper() {
-        String[] paths = {"/sync/getDatas", "/egovAtt/downloadEgovAttFile", "/openApi/wflow/getTodoAndToRead",
-                "/openApi/wflow/getTodoAndToReadCountList", "/openApi/receival/receiveDispatchCountWithUnit", "/openApi/receival/receiveDispatchWithUnit",
-                "/openApi/receival/getReceiveSignForPermission", "/userCenter/createTokenid", "/userCenter/userAuthorization",
-                "/openApi/urgerTaskSignUnit/getUrgerTaskSignByMap4Page", "/openApi/urger/setAppointmenJusticeMsg"};
-        return new IgnoredPathsWrapper(paths);
-    }
-
-    @Bean
-    IgnoredPathsWrapper mobileThemeIgnoredPathsWrapper() {
-        return new IgnoredPathsWrapper("/mobileTheme/getMobileHomeData", "/userCenter/getZzjgByTime");
-    }
-
-    /**
-     * 注意 initMethod = "init" 参数是必须的
-     * 方法名为 小写开头job名称 + Scheduler
-     *
-     * @param registryCenter
-     * @param todoOverdueJob 更新
-     * @return
-     */
-    @Bean(initMethod = "init")
-    public SpringJobScheduler todoOverdueJobScheduler(CoordinatorRegistryCenter registryCenter, TodoOverdueJob todoOverdueJob) {
-        // 定义作业核心配置 newBuilder的第1个参数为 小写开头job名称， 第2个参数为cron表达式， 第3个参数为分片数量， 同步骤4简单用法用1即可，高级用法见官方文档
-        JobCoreConfiguration jobCoreConfiguration = JobCoreConfiguration.newBuilder("todoOverdueJob", "0 0/30 * * * ? *", 1).build();
-        // 定义SIMPLE类型配置 固定写法， XxlJob换成你的Job类名
-        SimpleJobConfiguration simpleJobConfiguration = new SimpleJobConfiguration(jobCoreConfiguration, TodoOverdueJob.class.getCanonicalName());
-        // 定义Lite作业根配置 固定写法，高级用法见官方文档
-        LiteJobConfiguration liteJobConfiguration = LiteJobConfiguration.newBuilder(simpleJobConfiguration).overwrite(true).build();
-        return new SpringJobScheduler(todoOverdueJob, registryCenter, liteJobConfiguration);
-    }
-
-    @Bean(initMethod = "init")
-    public SpringJobScheduler todoReminJobScheduler(CoordinatorRegistryCenter registryCenter, TodoReminJob todoReminJob) {
-        // 定义作业核心配置 newBuilder的第1个参数为 小写开头job名称， 第2个参数为cron表达式， 第3个参数为分片数量， 同步骤4简单用法用1即可，高级用法见官方文档
-        JobCoreConfiguration jobCoreConfiguration = JobCoreConfiguration.newBuilder("todoReminJob", "0 0/30 * * * ? *", 1).build();
-        // 定义SIMPLE类型配置 固定写法， XxlJob换成你的Job类名
-        SimpleJobConfiguration simpleJobConfiguration = new SimpleJobConfiguration(jobCoreConfiguration, TodoReminJob.class.getCanonicalName());
-        // 定义Lite作业根配置 固定写法，高级用法见官方文档
-        LiteJobConfiguration liteJobConfiguration = LiteJobConfiguration.newBuilder(simpleJobConfiguration).overwrite(true).build();
-        return new SpringJobScheduler(todoReminJob, registryCenter, liteJobConfiguration);
-    }
-
-    @Bean(initMethod = "init")
-    public SpringJobScheduler todoCountRemindJobScheduler(CoordinatorRegistryCenter registryCenter, TodoCountRemindJob todoCountRemindJob) {
-        // 定义作业核心配置 newBuilder的第1个参数为 小写开头job名称， 第2个参数为cron表达式， 第3个参数为分片数量， 同步骤4简单用法用1即可，高级用法见官方文档
-        JobCoreConfiguration jobCoreConfiguration = JobCoreConfiguration.newBuilder("todoCountRemindJob", "0 * * * * ? *", 1).build();
-        // 定义SIMPLE类型配置 固定写法， XxlJob换成你的Job类名
-        SimpleJobConfiguration simpleJobConfiguration = new SimpleJobConfiguration(jobCoreConfiguration, TodoCountRemindJob.class.getCanonicalName());
-        // 定义Lite作业根配置 固定写法，高级用法见官方文档
-        LiteJobConfiguration liteJobConfiguration = LiteJobConfiguration.newBuilder(simpleJobConfiguration).overwrite(true).build();
-        return new SpringJobScheduler(todoCountRemindJob, registryCenter, liteJobConfiguration);
-    }
-
+// 注册一个bean到spring容器
+//    @Bean
+//    public RedisConnectionFactory jedisConnectionFactory() {
+//        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
+//                .master("mymaster")
+//                .sentinel("106.54.209.129", 6370);
+//        return new JedisConnectionFactory(sentinelConfig);
+//    }
 
 }
